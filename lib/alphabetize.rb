dd994ad74c542cbe7c147f51f552308048a1cb9e
Ryan Bravo <rrbravo2@gmail.com>
2019-06-01 22:12:01 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,4 @@
 def alphabetize(arr)
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by{|string| string.split('').map {|letter| ESPERANTO_ALPHABET.index(letter)}}# code here
+  arr.sort_by{|string| string.split('').map {|letter| "ESPERANTO_ALPHABET".index(letter)}}# code here
 end
